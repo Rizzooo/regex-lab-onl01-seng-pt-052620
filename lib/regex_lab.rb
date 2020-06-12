85b8feb535b0dccc99dd95c696992d946e249265
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-12 18:18:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/^un*(ing)$) == nil
+  if text.match(/\A(un*)(ing)$) == nil
     false
   else
     true
